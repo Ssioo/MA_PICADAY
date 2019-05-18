490d519ace92767a7377b5ab0ce851a0989bf3a5
Ssioo <41153640+Ssioo@users.noreply.github.com>
2019-05-18 13:36:11 +0900

    UI basic components
    main page

---
+++ app/src/main/java/com/pa1/picaday/Main_monthly_add.java
@@ -0,0 +1,13 @@
+package com.pa1.picaday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_monthly_add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_monthly_add);
+    }
+}
