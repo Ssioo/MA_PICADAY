f49410b2ed27bd9615b282afadd4c831b2a50997
Ssioo <41153640+Ssioo@users.noreply.github.com>
2019-06-04 18:01:04 +0900

    Timepicker function uploaded
    .

--- app/src/main/java/com/pa1/picaday/AddActivity_Fragment/AddActivity_daily.java
+++ app/src/main/java/com/pa1/picaday/AddActivity_Fragment/AddActivity_daily.java
@@ -9,8 +9,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.pa1.picaday.R;
+
+import org.w3c.dom.Text;
+
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class AddActivity_daily extends BottomSheetDialogFragment {
     public static AddActivity_daily getInstance() { return new AddActivity_daily(); }
@@ -39,6 +49,28 @@ public class AddActivity_daily extends BottomSheetDialogFragment {
             public void onClick(View v) {
 
                 dismiss();
+            }
+        });
+
+        /* TImePicker 구동 */
+        Date today = Calendar.getInstance().getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.getDefault());
+
+        TextView tp_start = (TextView) view.findViewById(R.id.start_timepicker_day);
+        tp_start.setText(sdf.format(today));
+        tp_start.setOnClickListener(new TextView.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        TextView tp_end = (TextView) view.findViewById(R.id.end_timepicker_day);
+        tp_end.setText(sdf.format(today));
+        tp_end.setOnClickListener(new TextView.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
         return view;
