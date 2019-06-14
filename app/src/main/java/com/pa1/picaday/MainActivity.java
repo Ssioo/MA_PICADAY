ac31423b7689a3707ec4f3fbcd160d5142b534f9
Ssioo <41153640+Ssioo@users.noreply.github.com>
2019-06-14 23:31:54 +0900

    UI Changed (Drawer Layout Added)
    .

--- app/src/main/java/com/pa1/picaday/MainActivity.java
+++ app/src/main/java/com/pa1/picaday/MainActivity.java
@@ -2,42 +2,55 @@ package com.pa1.picaday;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.pa1.picaday.AddActivity_Fragment.AddActivity_daily;
 import com.pa1.picaday.AddActivity_Fragment.AddActivity_monthly;
 import com.pa1.picaday.AddActivity_Fragment.AddActivity_weekly;
-import com.pa1.picaday.MainActivity_Fragment.MainActivity_daily;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private Toolbar toolbar;
+    private DrawerLayout drawerlayout;
+    private NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* 상단 TAB 레이아웃 구성 */
-        tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        /* Toolbar 구성 */
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        /* TAB 레이아웃 구성 */
+        tabLayout = findViewById(R.id.tabLayout);
         tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic2_week));
         tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic2_day));
         tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic2_month));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        /* 하단 메인 뷰페이저 구성 (DAILY, MONTHLY, WEEKLY 프래그먼트) */
+        /* 뷰페이저 구성 (DAILY, MONTHLY, WEEKLY 프래그먼트) */
         viewPager = findViewById(R.id.pager);
-
         TabPagerAdapter adapter = new TabPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount()); // TabPagerAdapter 호출로 뷰페이저 구성
         viewPager.setAdapter(adapter);
 
@@ -59,21 +72,38 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
+            public void onTabUnselected(TabLayout.Tab tab) { }
 
             @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
+            public void onTabReselected(TabLayout.Tab tab) { }
         });
 
-        /* ADD 버튼 눌렀을 때 */
-        ImageButton btn_add = (ImageButton) findViewById(R.id.btn_add);
-        btn_add.setOnClickListener(new ImageButton.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent;
+
+        /* Navigation Drawer 클릭 이벤트*/
+        drawerlayout = findViewById(R.id.drawerlayout);
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                drawerlayout = findViewById(R.id.drawerlayout);
+                if (!drawerlayout.isDrawerOpen(Gravity.START)) {
+                    drawerlayout.openDrawer(Gravity.START);
+                }
+                return true;
+            }
+
+            case R.id.action_add: {
                 switch (tabLayout.getSelectedTabPosition()) {
                     case 0:
                         AddActivity_weekly addActivity_weekly = AddActivity_weekly.getInstance();
@@ -90,28 +120,42 @@ public class MainActivity extends AppCompatActivity {
                     default:
                         break;
                 }
+                return true;
             }
-        });
-
-        /* MYPAGE 버튼 눌렀을 때*/
-        ImageButton btn_mypage = (ImageButton) findViewById(R.id.btn_mypage);
-        btn_mypage.setOnClickListener(new ImageButton.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SignIn_Activity.class);
-
-                startActivity(intent);
+            case R.id.action_auto: {
+                // 일정 자동 세팅
+                return true;
             }
-        });
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-        /* SETTING 버튼 눌렀을 때 */
-        ImageButton btn_setting = (ImageButton) findViewById(R.id.btn_setting);
-        btn_setting.setOnClickListener(new ImageButton.OnClickListener() {
-            public void onClick(View v) {
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Log.e("item", String.valueOf(menuItem.getItemId()));
+        switch (menuItem.getItemId()) {
+            /* Mypage */
+            case R.id.navigation_item_mypage:
+                break;
+            /* Setting page */
+            case R.id.navigation_item_setting:
                 Intent intent = new Intent(getApplicationContext(), Settingpage.class);
-
+                Toast.makeText(getApplicationContext(), "SETTING PAGE", Toast.LENGTH_SHORT).show();
                 startActivity(intent);
-            }
-        });
+                break;
+        }
 
+        //menuItem.setChecked(true);
+        drawerlayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        drawerlayout = findViewById(R.id.drawerlayout);
+        if (drawerlayout.isDrawerOpen(GravityCompat.START)) {
+            drawerlayout.closeDrawer(GravityCompat.START);
+        }
+        else {super.onBackPressed();}
     }
 }
